9d7c712db9e34d4c2602d61879733ea2c13dbe88
Dr8ane <99963376+Dr8ane@users.noreply.github.com>
2024-09-02 19:33:41 +0300

    Update task2.php

--- TestTask/Task3/task2.php
+++ TestTask/Task3/task2.php
@@ -1,6 +1,6 @@
 <?php
 // Подключение к базе данных
-$pdo = new PDO('mysql:host=localhost;dbname=test_tusk', 'root', '');
+$pdo = new PDO('mysql:host=evgens19.beget.tech;dbname=evgens19_test', 'evgens19_test', 'd8Jtpa%l');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Получение данных из базы данных для отображения
@@ -8,5 +8,48 @@ $stmt = $pdo->query("SELECT name_news, text_news, date_publication FROM news ORD
 $news_ = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // Подключение файла HTML
-include 'task2.html';
 ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styletask.css">
+    <title>Форма обратной связи</title>
+</head>
+<body>
+<header>
+    <nav>
+        <ul class="menu">
+            <div class="container">
+                <li><a href="task3.php">Главная</a></li>
+                <li><a href="task2.php">Новости</a></li>
+                <li><a href="task1.php">Обратная связь</a></li>
+            </div>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="table_content">
+        <h2>Новости:</h2>
+        <div class="container">
+            <table>
+                <tr>
+                    <th>Название</th>
+                    <th>Текст</th>
+                    <th>Дата публикации</th>
+                </tr>
+                <?php foreach ($news_ as $news): ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($news['name_news']); ?></td>
+                    <td><?php echo htmlspecialchars($news['text_news']); ?></td>
+                    <td><?php echo htmlspecialchars($news['date_publication']); ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </section>
+</main>
+</body>
+</html>
+
